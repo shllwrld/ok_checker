0d0f74c84d9baf5d0535f6bb4cd59a30a9d425e5
shllwrld <sayhelloworld@disroot.org>
2020-01-21 20:03:56 +0800

    added profile_info and profile_registration_date to output

--- ok_checker.py
+++ ok_checker.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import requests
 from bs4 import BeautifulSoup
@@ -15,19 +16,33 @@ def check_login(login_data):
     root_soup = BeautifulSoup(request.content, 'html.parser')
     soup = root_soup.find('div', {'data-l': 'registrationContainer,offer_contact_rest'})
     if soup:
-        masked_name = soup.find('div', {'class': 'ext-registration_tx taCenter'})
+        account_info = soup.find('div', {'class': 'ext-registration_tx taCenter'})
         masked_email = soup.find('a', {'data-l': 't,email'})
         masked_phone = soup.find('a', {'data-l': 't,phone'})
         if masked_phone:
             masked_phone = masked_phone.get('data-post')
         if masked_email:
             masked_email = masked_email.get('data-post')
-        if masked_name:
-            masked_name = masked_name.get_text()
+        if account_info:
+            masked_name = account_info.find('div', {'class': 'ext-registration_username_header'})
+            if masked_name:
+                masked_name = masked_name.get_text()
+            account_info = account_info.findAll('div', {'class': 'lstp-t'})
+            if account_info:
+                profile_info = account_info[0].get_text()
+                profile_registred = account_info[1].get_text()
+            else:
+                profile_info = None
+                profile_registred = None
+        else:
+            return None
+
         return {
             'masked_name': masked_name,
             'masked_email': masked_email,
             'masked_phone': masked_phone,
+            'profile_info': profile_info,
+            'profile_registred': profile_registred,
         }
 
     if root_soup.find('div', {'data-l': 'registrationContainer,home_rest'}):
