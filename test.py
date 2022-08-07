20a8c5cb2f0d93f5df68cdeb68647d88638af4da
shllwrld <shllwrld@users.noreply.github.com>
2022-08-07 09:51:54 +0800

    updated test.py due to server response changes

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from ok_checker import check_login
 
 def test():
     result = check_login('test@mail.ru')
-    assert result.get('masked_phone') == '99292984****'
-    assert result.get('masked_email') == 'te**@mail.ru'
+    assert result.get('masked_phone') == '992*******26'
     assert result.get('masked_name') == 'Ховар Х******'
     assert result.get('profile_registred') == 'Профиль создан 10 декабря 2014'
     assert result.get('profile_info') == "47 лет, Худжанд"
